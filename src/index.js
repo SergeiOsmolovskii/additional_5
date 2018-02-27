9a4a852249b3978fdad1299a64d289b1f4a97022
SergeiOsmolovskii <36640948+SergeiOsmolovskii@users.noreply.github.com>
2018-02-27 18:57:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+  return false;}
+   var strl = str.length;
+   var bracketsConfig = bracketsConfig.length;
+   var massiv = [];
+    for(let i = 0; i < strl; i++)
+    {
+      for(let n = 0; n < bracketsConfig; n++)
+      {
+        if (str[i] == bracketsConfig[n])
+        {
+        massiv.unshift(str[i]);
+     };
+        if (str[i] == bracketsConfig[n])
+        {
+          if (massiv[0] == bracketsConfi[n])
+          {
+            massiv.shift();
+          };
+        };
+      };
+    };
+  if (massiv.length !=0) 
+ {
+ return false; 
+  };
+    return true;
+  };
